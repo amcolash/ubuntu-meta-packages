3e5fc329bd45c086c8dd75fc7ce74c2648b5bcc0
Andrew McOlash <amcolash@gmail.com>
2023-06-04 00:48:14 -0700

    lots of cleanup work, better repo + key script, nicer test file

--- repositories/post-install.sh
+++ repositories/post-install.sh
@@ -1,49 +1,73 @@
 #!/bin/sh
 
+recv_key() {
+  KEY=$1
+  NAME=$2
+
+  echo Getting key for $NAME
+
+  if [ -z "$3" ]; then
+    KEYSERVER="hkps://keyserver.ubuntu.com"
+  else
+    KEYSERVER=$3
+  fi
+
+  gpg --keyserver $KEYSERVER --recv-keys $KEY
+  gpg --export $KEY > /etc/apt/trusted.gpg.d/$NAME.gpg
+  gpg --batch --yes --delete-keys $KEY
+}
+
+download_key() {
+  URL=$1
+  NAME=$2
+
+  echo Getting key for $NAME
+
+  curl -fsSL $URL | gpg --dearmor > /etc/apt/trusted.gpg.d/$NAME.gpg
+}
+
 ############ CORE ############
 
 # Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+download_key https://dl.google.com/linux/linux_signing_key.pub google
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
 ############ UI ############
 
 # Papirus Icon Theme
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9461999446FAF0DF770BFC9AE58A9D36647CAE7F
+recv_key 9461999446FAF0DF770BFC9AE58A9D36647CAE7F papirus
 echo "deb http://ppa.launchpad.net/papirus/papirus-dev/ubuntu jammy main" > /etc/apt/sources.list.d/papirus.list
 
 ############ DEV ############
 
 # node 18.x
-curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+download_key https://deb.nodesource.com/gpgkey/nodesource.gpg.key node18
 echo 'deb https://deb.nodesource.com/node_18.x jammy main' > /etc/apt/sources.list.d/nodesource.list
 
 # vs code
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-rm packages.microsoft.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+download_key https://packages.microsoft.com/keys/microsoft.asc microsoft
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 
 # docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+download_key https://download.docker.com/linux/ubuntu/gpg docker
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable" > /etc/apt/sources.list.d/docker-ce.list
 
 ############ MEDIA ############
 
 # spotify
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 5E3C45D7B312C643
+download_key https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg spotify
 echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list
 
 # darktable
-curl -L https://download.opensuse.org/repositories/graphics:darktable/xUbuntu_22.04/Release.key | apt-key add -
+download_key https://download.opensuse.org/repositories/graphics:darktable/xUbuntu_22.04/Release.key darktable
 echo "deb http://download.opensuse.org/repositories/graphics:/darktable/xUbuntu_22.04/ /" > /etc/apt/sources.list.d/darktable.list
 
 # mkvtoolnix
-curl -L https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt | apt-key add -
+download_key https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt mkvtoolnix
 echo "deb [arch=amd64] https://mkvtoolnix.download/ubuntu/ jammy main" > /etc/apt/sources.list.d/mkvtoolnix.list
 
 # steam
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
+recv_key F24AEA9FB05498B7 steam
 echo "deb [arch=amd64,i386] http://repo.steampowered.com/steam/ precise steam" > /etc/apt/sources.list.d/steam.list
 
 # High Quality Bluetooth Codecs
@@ -53,61 +77,40 @@ echo "deb [arch=amd64,i386] http://repo.steampowered.com/steam/ precise steam" >
 ############ UTIL ############
 
 # Arronax
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E53B0E36210D2EDBFA94E8AB5AF549300FEB6DD9
+recv_key E53B0E36210D2EDBFA94E8AB5AF549300FEB6DD9 arronax
 echo "deb http://ppa.launchpad.net/diesch/stable/ubuntu jammy main" > /etc/apt/sources.list.d/diesch.list
 
 # Slack
-curl -L https://packagecloud.io/slacktechnologies/slack/gpgkey | apt-key add -
+download_key https://packagecloud.io/slacktechnologies/slack/gpgkey slack
 echo "deb https://packagecloud.io/slacktechnologies/slack/debian/ jessie main" > /etc/apt/sources.list.d/slack.list
 
 # TeamViewer
-curl -L https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc | apt-key add -
-
-# Have to do this so this matches the package version
-cat > /etc/apt/sources.list.d/teamviewer.list << EOF
-###   TeamViewer DEB repository list
-
-### NOTE: Manual changes to this file
-###        - prevent it from being updated by TeamViewer package updates
-###        - will be lost after using the 'teamviewer repo' command
-###       The original file can be restored with this command:
-###       cp /opt/teamviewer/tv_bin/script/teamviewer.list /etc/apt/sources.list.d/teamviewer.list
-###       which has the same effect as 'teamviewer repo default'
-
-### NOTE: It is preferred to use the following commands to edit this file:
-###       teamviewer repo                - show current repository configuration
-###       teamviewer repo default        - restore default configuration
-###       teamviewer repo disable        - disable the repository
-###       teamviewer repo stable         - make all regular TeamViewer packages available (default)
-###       teamviewer repo preview        - additionally, make feature preview packages available
-###       teamviewer repo development    - additionally, make the latest development packages available
-
-
-deb http://linux.teamviewer.com/deb stable main
-
-# deb http://linux.teamviewer.com/deb preview main
-# deb http://linux.teamviewer.com/deb development main
-EOF
+download_key https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc teamviewer
+echo 'deb http://linux.teamviewer.com/deb stable main' > /etc/apt/sources.list.d/teamviewer.list
 
 # Albert
-curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_22.04/Release.key | apt-key add -
+download_key https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_22.04/Release.key albert
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_22.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list
 
-# Etcher
-curl -1sLf "https://dl.cloudsmith.io/public/balena/etcher/gpg.70528471AFF9A051.key" | apt-key add -
-echo "deb https://dl.cloudsmith.io/public/balena/etcher/deb/linuxmint ulyssa main" > /etc/apt/sources.list.d/balena-etcher.list
+# Etcher (Disabled because it's not working)
+# download_key "https://dl.cloudsmith.io/public/balena/etcher/gpg.70528471AFF9A051.key" etcher
+# echo "deb https://dl.cloudsmith.io/public/balena/etcher/deb/linuxmint ulyssa main" > /etc/apt/sources.list.d/balena-etcher.list
 
 # Discord
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 08633B4AAAEB49FC
+recv_key 08633B4AAAEB49FC discord
 echo "deb [arch=amd64] https://ppa.javinator9889.com all main" > /etc/apt/sources.list.d/javinator9889-ppa.list
 
 # Howdy
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 2802FD7D4CBF1B050BDE8F3D8CEBC97F3E9E1D58
+recv_key 2802FD7D4CBF1B050BDE8F3D8CEBC97F3E9E1D58 howdy
 echo "deb http://ppa.launchpad.net/boltgolt/howdy/ubuntu jammy main" > /etc/apt/sources.list.d/boltgolt-howdy-focal.list
 
 # Tuxedo
-curl -fsSL https://deb.tuxedocomputers.com/0x54840598.pub.asc | apt-key add -
+download_key https://deb.tuxedocomputers.com/0x54840598.pub.asc tuxedo
 echo "deb https://deb.tuxedocomputers.com/ubuntu jammy main" > /etc/apt/sources.list.d/tuxedo.list
+
+# Dropbox
+recv_key 1C61A2656FB57B7E4DE0F4C1FC918B335044912E dropbox pgp.mit.edu
+echo "deb https://linux.dropbox.com/ubuntu jammy main" > /etc/apt/sources.list.d/dropbox.list
 
 ############ FINISHING UP ############
 
--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Go to script root
 pushd $(dirname "$0")
 
-docker-compose down
-docker-compose build
+# Build deb files if needed
 ../build.sh
-docker-compose run ubuntu bash -c /deb/install.sh
 
+# Clean up last container if things were not successful
+docker-compose down
+
+# Rebuild image and run install
+docker-compose build
+docker-compose run ubuntu_meta bash -c /deb/install.sh
+
+# Clean up after successful run
+docker-compose down
+
+# Go back to previous directory
 popd
